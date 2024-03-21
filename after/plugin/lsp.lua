3ee321f1afc86beff3a541b7c1ccbcd64c71c518
Thiago Mônaco <thiagomonaco98@gmail.com>
2024-03-21 15:13:01 -0300

    add config

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,64 @@
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+-- to learn how to use mason.nvim with lsp-zero
+-- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {
+	'lua_ls',
+	'tsserver',
+	'clangd',
+	'cssls',
+	'dockerls',
+	'docker_compose_language_service',
+	'eslint',
+	'html',
+	--'java_language_server'
+
+  },
+  handlers = {
+    lsp_zero.default_setup,
+  }
+})
+
+
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    -- `Enter` key to confirm completion
+    -- Ctrl+Space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+
+    -- Navigate between snippet placeholder
+    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+
+    -- Scroll up and down in the completion documentation
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+
+    ["<CR>"] = cmp.mapping(function(fallback)
+      -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
+      if cmp.visible() then
+        local entry = cmp.get_selected_entry()
+	if not entry then
+	  cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+	end
+	cmp.confirm()
+      else
+        fallback()
+      end
+    end, {"i","s","c",}),
+  })
+})
+
+
+
---
+++ lua/thiago/commands.lua
@@ -0,0 +1,2 @@
+vim.api.nvim_command('command! -nargs=0 RemoveLogs %g/console\\.log/d_')
+
