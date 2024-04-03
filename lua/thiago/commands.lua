be2e94855aebdddd7c92f4f7d3512f1d77d47084
Thiago Mônaco <thiagomonaco98@gmail.com>
2024-04-02 21:54:46 -0300

    adding command to see error detail

--- lua/thiago/commands.lua
+++ lua/thiago/commands.lua
@@ -1,2 +1,2 @@
 vim.api.nvim_command('command! -nargs=0 RemoveLogs %g/console\\.log/d_')
-
+vim.api.nvim_command('command! -nargs=0 Erdetail lua vim.diagnostic.open_float(0, {scope = "line", source = "always"})')
